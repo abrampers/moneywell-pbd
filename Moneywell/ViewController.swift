3e738b52119c287d0ebf0bb4ee11ca8d4c75c1c1
Abram Situmorang <abram.perdanaputra@gmail.com>
2019-02-18 13:19:56 +0700

    Initial Commit

---
+++ Moneywell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moneywell
+//
+//  Created by Abram Situmorang on 18/02/19.
+//  Copyright © 2019 Abram Situmorang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
